044875d979d5a9e77091dc79734dd451a27db370
Lukas Klassen <inf4143@hs-worms.de>
2024-12-16 14:03:20 +0100

    day4.2

--- 4-2.py
+++ 4-2.py
@@ -6,9 +6,6 @@ class Coords:
     def __init__(self, x, y):
         self.x = x
         self.y = y
-    
-    def __repr__(self) -> str:
-        return f"Coords({self.x},{self.y})"
 
 def loadInput() -> list[str]:
     return open("4.txt").read().splitlines()
@@ -16,11 +13,17 @@ def loadInput() -> list[str]:
 def findAllACoords(inputList: list[str]) -> list[Coords]:
     return [Coords(match.start(), y) for y, row in enumerate(inputList) for match in re.finditer(A, row)]
 
-def findMatch(inputList: list[str], coords: Coords):
-    if not 0 < coords.y < len(inputList)-1 and not 0 < coords.x+1 < len(inputList[coords.y]): return False
-    return 
+def findMatch(input: list[str], c: Coords):
+    if not 0 < c.y < len(input)-1 or not 0 < c.x < len(input[c.y])-1: return False
+    return isMS(input[c.y-1][c.x-1], input[c.y+1][c.x+1]) and isMS(input[c.y-1][c.x+1], input[c.y+1][c.x-1])
+
+def isMS(string1: str, string2: str) -> bool:
+    return string1+string2 in ["MS", "SM"]
+
+def sumMatches(inputList: list[str], aCoords: list[Coords]) -> int:
+    return sum(findMatch(inputList, coords) for coords in aCoords)
 
 if __name__ == '__main__':
     inputList = loadInput()
     allACoords = findAllACoords(inputList)
-    print(allACoords)
+    print(sumMatches(inputList, allACoords))
